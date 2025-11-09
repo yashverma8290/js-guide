def18ba3d5d23aa07f25820688154bc18e9e14a8
yashverma8290 <vermayash3205@gmail.com>
2025-11-09 22:25:55 +0530

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+let x = 10
+console.log(x)
+
+
+// number => 2 to power 3
+// bigint
+// string => "hello"
+// boolean => true/false
+// null => absence of value(standalone value)
+// undefined => variable declared but not assigned
+// symbol => unique identifier
+// object => { key: value }
+
+
+// object
+
+console.log(typeof x) 
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof "hello")
+console.log(typeof true)
+console.log(typeof 10n)
+console.log(typeof Symbol("id"))
+console.log(typeof { name: "Alice" })
---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445
+let accountEmail = "yash@gmail.com"
+var accountPassword =  "12345"
+accountCity = "jaipur"
+
+// accountId = 2
+
+accountEmail = "verma@gmail.com"
+accountPassword = "1212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword, accountCity ])
