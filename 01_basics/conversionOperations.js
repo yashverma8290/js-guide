6dd65e969af05cedf365e162bf19155e73d3de72
yashverma8290 <vermayash3205@gmail.com>
2025-11-11 06:41:52 +0530

    comaprison operator updated

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,39 +1,72 @@
-let score = ""
+// let score = " 33.4"
 
-console.log(typeof score)
-console.log(typeof(score))
-
-
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
-
-// "33abc" => 33
-// "abc33" => NaN
-// "100" => 100
-// "" => 0
-// " " => 0
-// "33.44" => 33.44
+// console.log(typeof score)
+// console.log(typeof(score))
 
 
-let isLoogedIn = NaN
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let booleanIsLoggedIn = Boolean(isLoogedIn)
-console.log(booleanIsLoggedIn);
-
-// "abc" => true
-// "" => false
-// 1 => true
-// 0 => false
-// 33 => true
-// null => false
-// undefined => false
-// NaN => false
+// // "33abc" => 33
+// // "abc33" => NaN
+// // "100" => 100
+// // "" => 0
+// // " " => 0
+// // "33.44" => 33.44
 
 
+// let isLoogedIn = ""
 
-let someNumber = 33
+// let booleanIsLoggedIn = Boolean(isLoogedIn)
+// console.log(booleanIsLoggedIn);
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// // "abc" => true
+// // " " => true
+// // "" => false
+// // 1 => true
+// // 0 => false
+// // 33 => true
+// // null => false
+// // undefined => false
+// // NaN => false
+
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// **************** Operations ******************//
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let temp1 = "1" + 2
+let temp2 = 1 + "2"
+let temp3 = "1" + 2 + 2
+let temp4 = 1 + 2 + "2"
+
+console.log(temp1);
+console.log(typeof temp1);
+
+console.log(temp2);
+console.log(typeof temp2);
+
+console.log(temp3);
+console.log(typeof(temp3));
+
+console.log(temp4);
+console.log(typeof(temp4));
